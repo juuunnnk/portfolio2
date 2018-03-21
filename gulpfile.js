742942d001ff0aaa115553d2615fc86869b9d71b
junki <junki@ryden.co.jp>
2018-03-21 23:50:41 +0900

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass')
+var autoprefixer = require('gulp-autoprefixer');
 var minifycss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
+var babel = require('gulp-babel');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var mozjpeg = require('imagemin-mozjpeg');
 var svgo = require('gulp-svgo');
-var autoprefixer = require('gulp-autoprefixer');
 var plumber = require('gulp-plumber');
 var pleeease = require('gulp-pleeease');
-var babel = require('gulp-babel');
 
-gulp.task('sass', function() {
+//compile
+gulp.task('compile', function () {
+  //js
+  gulp.src("./src/js/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js/'));
+
+  //css
   gulp.src("./sass/style.scss")
     .pipe(plumber())
     .pipe(sass({
@@ -20,17 +27,13 @@ gulp.task('sass', function() {
     .pipe(pleeease({
       autoprefixer: {
         browsers: ['last 2 versions']
-      }}))
+      }
+    }))
     .pipe(gulp.dest('css/'));
 });
 
-gulp.task('minify-js', function() {
-  return gulp.src("./src/js/*.js")
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js/'));
-});
-
-gulp.task("imageMinTask", function() {
+//画像圧縮
+gulp.task("imageMinTask", function () {
   gulp.src("./images/*.{png,jpg,svg}")
     .pipe(imagemin([
       pngquant({
@@ -47,8 +50,7 @@ gulp.task("imageMinTask", function() {
     .pipe(gulp.dest("dist/images/"));
 });
 
-gulp.task('default', function() {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-  gulp.watch('./src/js/*.js', ['minify-js']);
+gulp.task('default', function () {
+  gulp.watch(['./sass/**/*.scss', './src/js/*.js'],['compile']);
   gulp.watch('./src/images/*.{png,jpg,svg}', ['imageMinTask']);
 });
