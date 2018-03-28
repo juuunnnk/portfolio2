b4efb0ffb313e35e7df75c374043d87d735939f7
junk <junk.ien.zanmai@gmail.com>
2018-03-29 02:24:37 +0900

    スクロールアニメ調整２

--- src/js/components/scrollAnime.js
+++ src/js/components/scrollAnime.js
@@ -1,24 +1,41 @@
 import $ from "jquery";
 
+
 $(function () {
-    // $(window).ready(function () {
-    //     var worksPosition = $(this).offset().top;
-    //     var scroll = $(window).scrollTop();
-    //     var windowHeight = $(window).height();
-    //     $('.l-works__item').each(function () {
-    //         if (worksPosition > windowHeight - windowHeight / 5) {
-    //             $(this).css('opacity', '1.0');
-    //         }
-    //     })
-    // })
-    $(window).scroll(function () {
-        var worksPosition = $(this).offset().top;
-        var scroll = $(window).scrollTop();
-        var windowHeight = $(window).height();
-        $('.l-works__item').each(function () {
-            if (scroll > worksPosition - windowHeight + windowHeight / 5) {
-                $(this).addClass("fadeIn")
+    $(window).on('load scroll', function () {
+        setAnimationClass();
+    })
+    var animationClass = 'effect'; // アニメーション対象のclass名
+    var animationFinClass = 'fadeIn'; // アニメーションが実行時に付与するclass名
+    var animationStartPosition = 0.75; // アニメーションが実行されるスクロール位置
+
+    // アニメーションが完了しているかどうかを管理する配列
+    var animationFlagArr = [];
+    for (var i = 0; i < $('.' + animationClass).length; i++) {
+        animationFlagArr[i] = false;
+    }
+
+    function setAnimationClass() {
+        // アニメーションの実行前に、アニメーション完了済の要素を確認しておく
+        for (var i = 0; i < $('.' + animationClass).length; i++) {
+            if ($('.' + animationClass).eq(i).hasClass(animationFinClass)) {
+                animationFlagArr[i] = true;
             }
-        });
-    });
-});
+        }
+
+        // スクロール位置と画面高さを取得
+        var scrollTop = $(window).scrollTop();
+        var windowHeight = $(window).height();
+
+        // アニメーション対象要素が開始位置までスクロールしている場合、classを付与する
+        for (var i = 0; i < animationFlagArr.length; i++) {
+            if (animationFlagArr[i] == false) {
+                var thisElement = $('.' + animationClass).eq(i);
+                var thisTop = thisElement.offset().top;
+                if (scrollTop + (windowHeight * animationStartPosition) > thisTop) {
+                    thisElement.addClass(animationFinClass);
+                }
+            }
+        }
+    }
+})
